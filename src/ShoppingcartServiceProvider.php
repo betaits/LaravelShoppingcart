9764d53396c2af1e110caea358deb1200923ae07
betaits <betaitservices@gmail.com>
2019-03-23 22:31:56 -0400

    Update ShoppingcartServiceProvider.php

--- src/ShoppingcartServiceProvider.php
+++ src/ShoppingcartServiceProvider.php
@@ -2,11 +2,11 @@
 
 namespace Gloudemans\Shoppingcart;
 
-use Illuminate\Auth\Events\Logout;
 use Illuminate\Session\SessionManager;
 use Illuminate\Support\ServiceProvider;
+use Gloudemans\Shoppingcart\Cart;
 
-class ShoppingcartServiceProvider extends ServiceProvider
+class ServicesProvider
 {
 
     /**
@@ -14,28 +14,10 @@ class ShoppingcartServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register($container)
     {
-        $this->app->bind('cart', 'Gloudemans\Shoppingcart\Cart');
-
-        $config = __DIR__ . '/../config/cart.php';
-        $this->mergeConfigFrom($config, 'cart');
-
-        $this->publishes([__DIR__ . '/../config/cart.php' => config_path('cart.php')], 'config');
-
-        $this->app['events']->listen(Logout::class, function () {
-            if ($this->app['config']->get('cart.destroy_on_logout')) {
-                $this->app->make(SessionManager::class)->forget('cart');
-            }
-        });
-
-        if ( ! class_exists('CreateShoppingcartTable')) {
-            // Publish the migration
-            $timestamp = date('Y_m_d_His', time());
-
-            $this->publishes([
-                __DIR__.'/../database/migrations/0000_00_00_000000_create_shoppingcart_table.php' => database_path('migrations/'.$timestamp.'_create_shoppingcart_table.php'),
-            ], 'migrations');
+        $container['cart'] = function ($c) {
+            return new Cart($c);
         }
     }
 }
